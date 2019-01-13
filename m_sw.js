a92dc7f47334bb5707f6179b59bb86addd07590e
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-01-12 21:31:58 -0500

    Optimized site.

--- m_sw.js
+++ m_sw.js
@@ -1,34 +1,118 @@
-var CACHE_NAME = 'zjs';
-var urlsToCache = [
-  '/',
-  '/projects',
-];
-self.addEventListener('install', function(event) {
-  // Perform install steps
+/* global self, caches, fetch, URL, Response */
+'use strict';
+
+var config = {
+  version: 'achilles',
+  staticCacheItems: [
+    '/projects',
+    '/blog',
+    '/archives',
+    '/'
+  ],
+  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
+  offlineImage: '<svg role="img" aria-labelledby="offline-title"'
+    + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
+    + '<title id="offline-title">Offline</title>'
+    + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
+    + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
+    + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
+  offlinePage: '/offline/'
+};
+
+function addToCache (cacheKey, request, response) {
+  if (response.ok) {
+    var copy = response.clone();
+    caches.open(cacheKey).then( cache => {
+      cache.put(request, copy);
+    });
+  }
+  return response;
+}
+
+function fetchFromCache (event) {
+  return caches.match(event.request).then(response => {
+    if (!response) {
+      throw Error(`${event.request.url} not found in cache`);
+    }
+    return response;
+  });
+}
+
+function offlineResponse (resourceType, opts) {
+  if (resourceType === 'image') {
+    return new Response(opts.offlineImage,
+      { headers: { 'Content-Type': 'image/svg+xml' } }
+    );
+  } else if (resourceType === 'content') {
+    return caches.match(opts.offlinePage);
+  }
+  return undefined;
+}
+
+self.addEventListener('install', event => {
+  function onInstall (event, opts) {
+    return caches.open('static')
+      .then(cache =>
+        cache.addAll(opts.staticCacheItems)
+      );
+  }
+
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    onInstall(event, config)
   );
 });
-self.addEventListener('fetch', (event) => {
-  if (event.request.method === 'GET') {
-    event.respondWith(
-      caches.match(event.request)
-      .then((cached) => {
-        var networked = fetch(event.request)
-          .then((response) => {
-            let cacheCopy = response.clone()
-            caches.open(CACHE_NAME)
-              .then(cache => cache.put(event.request, cacheCopy))
-            return response;
-          })
-          .catch(() => caches.match(offlinePage));
-        return cached || networked;
-      })
-    )
+
+self.addEventListener('activate', event => {
+
+});
+
+self.addEventListener('fetch', event => {
+
+  function shouldHandleFetch (event, opts) {
+    var request            = event.request;
+    var url                = new URL(request.url);
+    var criteria           = {
+      matchesPathPattern: !!(opts.cachePathPattern.exec(url.pathname)),
+      isGETRequest: request.method === 'GET',
+      isFromMyOrigin    : url.origin === self.location.origin
+    };
+    var failingCriteria    = Object.keys(criteria)
+      .filter(criteriaKey => !criteria[criteriaKey]);
+    return !failingCriteria.length;
   }
-  return;
+
+  function onFetch (event, opts) {
+    var request = event.request;
+    var acceptHeader = request.headers.get('Accept');
+    var resourceType = 'static';
+    var cacheKey;
+
+    if (acceptHeader.indexOf('text/html') !== -1) {
+      resourceType = 'content';
+    } else if (acceptHeader.indexOf('image') !== -1) {
+      resourceType = 'image';
+    }
+
+    cacheKey = resourceType;
+
+    if (resourceType === 'content') {
+      event.respondWith(
+        fetch(request)
+          .then(response => addToCache(cacheKey, request, response))
+          .catch(() => fetchFromCache(event))
+          .catch(() => offlineResponse(resourceType, opts))
+      );
+    } else {
+      event.respondWith(
+        fetchFromCache(event)
+          .catch(() => fetch(request))
+            .then(response => addToCache(cacheKey, request, response))
+          .catch(() => offlineResponse(resourceType, opts))
+      );
+    }
+  }
+
+  if (shouldHandleFetch(event, config)) {
+    onFetch(event, config);
+  }
 });
