8f2a1a8b43211f864565ee3612fac5a5cb9b2967
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-07-30 07:37:22 -0400

    Work

--- test.sh
+++ test.sh
@@ -6,7 +6,14 @@ else
   echo "Python 3 not found. Testing for generic Python installation."
   if which python | grep -q 'python' ; then
     printf "Python found. Checking version ... "
-    $out = python -c "from sys import version; print version"
+    out=$(python -c "from sys import version; print version")
+    if [[ $out == *"3."* ]] ; then
+        printf "Found generic Python 3. "
+    else
+        if [[ $out == *"2."* ]] ; then
+            printf "Found generic Python 2. "
+        fi
+    fi
     echo "Done."
   fi
 fi
