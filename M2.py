c85b9bc32bf33b5fc8f469698c07320afbc7c7f8
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-08-10 07:59:22 -0400

    Test files modified.

--- M2.py
+++ M2.py
@@ -8,7 +8,7 @@ m = Markdown()
 
 open("out.html", "w").close()
 
-with open("./Content/Complete Overhaul.txt", "r") as fd, open("out.html", "a") as o_fd:
+with open("./Test.txt", "r") as fd, open("out.html", "a") as o_fd:
     for line in fd:
-        sleep(0.1)
+        # sleep(0.1)
         o_fd.write(m.html(line)+'\n')
