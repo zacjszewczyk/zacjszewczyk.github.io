be26f5008bdf9ceaddc50a91af6b399dae32384e
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-08-11 18:44:23 -0400

    Tried threading.

--- M2.py
+++ M2.py
@@ -1,14 +1,32 @@
 #!/usr/local/bin/python3
 
 from Markdown2 import Markdown
-from time import sleep
+import datetime
+from os import listdir
+import threading
+import concurrent.futures
 
-# m = Markdown("https://zacs.site/")
-m = Markdown()
+thread_local = threading.local()
 
-open("out.html", "w").close()
+def get_m():
+    if not hasattr(thread_local, "m"):
+        thread_local.m = Markdown()
+    return thread_local.m
 
-with open("./Test.txt", "r") as fd, open("out.html", "a") as o_fd:
-    for line in fd:
-        # sleep(0.1)
-        o_fd.write(m.html(line)+'\n')
+def handle_file(tgt):
+    m = get_m()
+    open("/Users/zjszewczyk/Desktop/ThreadTest/"+tgt.replace(".txt", ".html"), "w").close()
+    with open("./Content/"+tgt, "r") as fd, open("/Users/zjszewczyk/Desktop/ThreadTest/"+tgt.replace(".txt", ".html"), "a") as o_fd:
+        for line in fd:
+            o_fd.write(m.html(line)+'\n')
+
+t1 = datetime.datetime.now()
+
+for each in listdir("./Content"):
+    if (".txt" in each):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(handle_file, each)
+        # handle_file(each)
+
+t2 = datetime.datetime.now()
+print(("Execution time: "+str(t2-t1)+"s"))
