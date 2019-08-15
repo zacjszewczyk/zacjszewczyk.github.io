7a491322ca919367b3445e11d9dc77e9e3832009
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-08-15 06:47:48 -0400

    Oops.

--- blog.py
+++ blog.py
@@ -20,6 +20,7 @@ from multiprocessing import Pool # Multiprocessing
 ## - md: Placeholder for instance of Markdown parser. (Class)
 files = {}
 content = ""
+months = {"01":"January","02":"February","03":"March","04":"April","05":"May","06":"June","07":"July","08":"August","09":"September","10":"October","11":"November","12":"December"}
 md = ""
 
 # Config variables, read in from './.config'
