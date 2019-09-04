6fb1db1ef945b669b98c62a9bed406a350e6e1d7
Zac Szewczyk <zacjszewczyk@users.noreply.github.com>
2019-09-04 10:04:15 -0400

    Revamped CLI @ work.

--- blog.py
+++ blog.py
@@ -1,15 +1,217 @@
 #!/usr/local/bin/python3
 
+## BEGIN CLI BLOCK
+# Purpose: The purpose of this block is to provide an easy to use, as modular
+# as possible command line interface. Simply paste in this block, then edit.
+
+### Import modules
+from sys import exit, argv, stdout, stdin # Command line interface
+from tty import setraw, setcbreak # Raw input
+from termios import tcgetattr, tcsetattr, TCSAFLUSH # Backup/resume shell
+from os.path import exists # Reading input files
+
+# Class: c(olors)
+# Purpose: provide easy access to ANSI escape codes for styling output
+class c():
+    HEADER = '\033[95m' # Pink
+    OKBLUE = '\033[94m' # Purple
+    OKGREEN = '\033[92m' # Green
+    WARNING = '\033[93m' # Yellow
+    FAIL = '\033[91m' # Red
+    ENDC = '\033[0m' # None
+    BOLD = '\033[1m' # Blue
+    UNDERLINE = '\033[4m' # Underline
+
+# Method: ActivateInterface
+# Purpose: Activate the command line interface.
+# Parameters: none.
+def ActivateInterface():
+    # If the user just runs the file, notify them that they can use "-a"
+    # to enter "Authoring Mode". Then build the site.
+    if (len(argv) == 1):
+        print(c.UNDERLINE+"Note"+c.ENDC+": You can use '-a' to enter 'Authoring Mode'")
+    # If they have run the program with a single parameter, bounds check
+    # it, then send them to the interface
+    elif (len(argv) == 2):
+        # Don't allow double or single quatation marks, or input over 2
+        # characters long
+        if ('"' in argv[1] or "'" in argv[1] or len(argv[1]) > 2):
+            print(c.FAIL+"Invalid parameters"+c.ENDC)
+            exit(0)
+
+        # Send the user to the CLI
+        DisplayInterface(argv[1:])
+    
+    # Allow the user to search from the CLI
+    elif (len(argv) == 3 and argv[1] == "-S"):
+        DisplayInterface(argv[1],argv[2],"quit")
+
+    else: # Too many parameters
+        print(c.FAIL+"Too many parameters"+c.ENDC)
+        exit(0)
+
+# Method: DisplayInterface
+# Purpose: Provide a command line interface for the script, for more granular control
+# of its operation.
+# Parameters: params: command line parameters (String)
+def DisplayInterface(params,search_query="",end_action="continue"):
+    # If the exit flag is set, check for other flags, then exit.
+    if ("--exit" in params):
+        end_action = "quit"
+
+    # Store the menu in a variable so as to provide easy access at any point in time.
+    menu = """
+    * To search all articles:                        %s-S%s
+    * To revert post timestamps:                     %s-r%s
+    * To clear all structure files:                  %s-R%s
+    * To display this menu:                          %s-h%s
+    * To exit this mode and build the site:          %sexit%s
+    * To exit this mode and quit the program:        %s!exit%s
+    """ % (c.OKGREEN, c.ENDC, c.OKGREEN, c.ENDC, c.OKGREEN, c.ENDC, c.WARNING, c.ENDC, c.FAIL, c.ENDC, c.FAIL, c.ENDC)
+
+    # Continue prompting the user for input until they enter a valid argument
+    while (True):
+        if ("-a" in params): # Get new input
+            print(menu)
+            params = GetUserInput("#: ")
+        if ("-h" in params or "help" in params): # Print help menu
+            print('Entering "-h" at any time will display the menu below.\n%s' % (menu))
+        elif ("-S" in params): # Search all articles
+            # If one has not been provided, get a string to search all files for
+            if (search_query == ""):
+                search_query = GetUserInput("Enter search string: ")
+            # Iterate over the entire ./Content dirctory
+            for file in listdir("Content"):
+                # Only inspect text files
+                if (not ".txt" in file):
+                    continue
+                # Search each line of the file, case insensitively
+                res = SearchFile(file, search_query)
+                if (res):
+                    print("\nFile: "+c.UNDERLINE+file+c.ENDC)
+                    for match in res:
+                        print("    %sLine %d:%s %s" % (c.BOLD, match[0], c.ENDC, match[1]))
+            # Cleanup
+            del res
+        elif ("-r" in params): # Revert post timestamps
+            for files in listdir("Content"):
+                if (files.endswith(".txt")):
+                    Revert("Content/"+files)
+        elif ("-R" in params): # Rebuild all structure files
+            for files in listdir("./local/blog"):
+                if (files.endswith(".html")):
+                    remove("./local/blog/"+files)
+        elif ("!exit" in params): # Exit without building site
+            exit(0)
+        else: # Exit then build site
+            return False
+        
+        if (end_action == "continue"):
+            params = GetUserInput("#: ")
+        else:
+            # Cleanup
+            del menu
+            exit(0)
+
+# Method: GetUserInput
+# Purpose: Accept user input and perform basic bounds checking
+# Parameters:
+# - prompt: Text to prompt the user for input (String)
+def GetUserInput(prompt):
+    # Prompt the user for valid input until they enter it.
+    while True:
+        # Capture user input
+        string = GetLine(prompt)
+
+        # Bounds check
+        ## Do not allow empty strings
+        if (len(string) == 0):
+            print(c.WARNING+"Input cannot be empty."+c.ENDC)
+            continue
+        ## Do not allow more than 256 characters
+        elif (len(string) > 256):
+            print(c.WARNING+"Input bound exceeded."+c.ENDC)
+            continue
+        
+        # If we get here, we have valid input
+        break
+        
+    # Send the user's input back to the requesting method.
+    return string
+
+# Method: GetLine
+# Purpose: Read a line from the user, allowing for cursor movement.
+# Parameters: 
+# - prompt: Text to prompt the user for input (String)
+def GetLine(prompt):
+    # Backup the shell session, to restore it later.
+    backup = tcgetattr(stdin)
+
+    # Set the stage for the session.
+    setraw(stdin)
+    input = ""
+    index = 0
+    
+    # Continue accepting input until the user aborts the process (CTRL-C)
+    # or hits the enter key.
+    while True:
+        # Clear screen, then print current input string with prompt
+        stdout.write(u"\u001b[1000D")
+        stdout.write(u"\u001b[0K")
+        stdout.write(prompt+" "+input)
+        stdout.write(u"\u001b[1000D")
+        stdout.write(u"\u001b[" + str(index+len(prompt)+1) + "C")    
+        stdout.flush()
+        
+        # Read a single character and get its code.
+        char = ord(stdin.read(1))
+        
+        # Manage internal data-model
+        if char == 3: # CTRL-C
+            # Restore shell session from backup, then exit.
+            tcsetattr(stdin, TCSAFLUSH, backup)
+            return
+        elif 32 <= char <= 126: # Normal letters
+            # Insert new characters at the appropriate index.
+            input = input[:index] + chr(char) +  input[index:]
+            index += 1
+        elif char in {10, 13}: # Enter key
+            # Restore shell session from backup, then exit the loop.
+            # index = 0 # Should be able to get rid of this.
+            tcsetattr(stdin, TCSAFLUSH, backup)
+            break
+        elif char == 27: # Arrow keys
+            # Accept input from arrow keys and move the cursor accordingly
+            next1, next2 = ord(stdin.read(1)), ord(stdin.read(1))
+            if next1 == 91:
+                if next2 == 68: # Left
+                    index = max(0, index - 1)
+                elif next2 == 67: # Right
+                    index = min(len(input), index + 1)
+        elif char == 127: # Delete
+            # Remove characters at the appropriate index.
+            input = input[:index-1] + input[index:]
+            index = max(index-1,0)
+        # stdout.flush() # Should be able to get rid of this.
+
+    # On exit, print a newline and reset the cursor left.
+    stdout.write('\n')
+    stdout.write(u"\u001b[1000D")
+    
+    # Cleanup
+    del index, char
+    
+    # Return the string the user's input.
+    return input
+## END CLI BLOCK
+
 # Imports
 from os import listdir, stat, remove, utime, mkdir # File/folder operations
 from os.path import isdir, isfile # File/folder existence operations
 from time import strptime, strftime, mktime, localtime, gmtime # Managing file mod time
 from datetime import datetime # Recording runtime
-from re import search # Regex -- Get this out!
-from sys import exit, argv, stdout, stdin # Command line options
 from Markdown2 import Markdown # Markdown parser
 from ModTimes import CompareMtimes # Compare file mod times
-from colors import c # Output styling
 from multiprocessing import Pool # Multiprocessing
 from random import choices # Building Explore page
 
@@ -962,30 +1164,7 @@ def Terminate():
 # If run as an individual file, generate the site and report runtime.
 # If imported, only make methods available to imported program.
 if __name__ == '__main__':
-    # If the user just runs the file, notify them that they can use "-a"
-    # to enter "Authoring Mode". Then build the site.
-    if (len(argv) == 1):
-        print(c.UNDERLINE+"Note"+c.ENDC+": You can use '-a' to enter 'Authoring Mode'")
-    # If they have run the program with a single parameter, bounds check
-    # it, then send them to the interface
-    # This needs work! Make it more like ReadLater's implementation.
-    elif (len(argv) == 2):
-        # Don't allow double or single quatation marks, or input over 2
-        # characters long
-        if ('"' in argv[1] or "'" in argv[1] or len(argv[1]) > 2):
-            print(c.FAIL+"Invalid parameters"+c.ENDC)
-            exit(0)
-
-        # Send the user to the CLI
-        Interface(argv[1:])
-    
-    # Allow the user to search from the CLI
-    elif (len(argv) == 3 and argv[1] == "-S"):
-        Interface(argv[1],argv[2],"exit")
-
-    else:
-        print(c.FAIL+"Too many parameters"+c.ENDC)
-        exit(0)
+    ActivateInterface()
 
     t1 = datetime.now()
     Init()
