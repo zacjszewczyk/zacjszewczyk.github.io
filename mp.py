b70d969027f7d5a4f9199cab0cdb1ed8dc41f051
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-12-07 17:17:57 -0500

    Work

--- mp.py
+++ mp.py
@@ -19,12 +19,71 @@ class Orchestrator:
 
     def __init__(self, o):
         self.md = self.Markdown("https://zacs.site")
+        # # Year
+        # print(o[0])
+        # for month in o[1]:
+        #     # Month
+        #     print("  "+o[0]+"-"+month)
+        #     for day in o[1][month]:
+        #         # Day
+        #         print("    "+o[0]+"-"+month+"-"+day)
+        #         for timestamp in o[1][month][day]:
+        #             # Individual articles by time
+        #             print("      "+o[0]+"-"+month+"-"+day+"-"+timestamp+"_"+o[1][month][day][timestamp])
 
-        print(o[0])
-        for month in o[1]:
-            print("  "+o[0]+"-"+month)
-            for day in o[1][month]:
-                print("    "+o[0]+"-"+month+"-"+day)
+
+        # For each year in which a post was made, generate a 'year' file, that
+        # contains links to each month in which a post was published.
+
+        # Clear the 'year' file
+        open("./local/blog/"+o[0]+".html", "w", encoding=ENCODING).close()
+        year_fd = open("./local/blog/"+o[0]+".html", "a", encoding=ENCODING)
+        # Write the opening HTML tags
+        year_fd.write(content[2].replace("{{ title }}", "Post Archives - ").replace("{{ BODYID }}", "archives", 1))
+        # Display the months listed.
+        year_fd.write("<div id=\"years_index\">\n<div>%s</div>\n" % (o[0]))
+        # Sort the sub-dictionaries by keys, months, then iterate over it. For each
+        # month in which a post was made, generate a 'month' file that contains all
+        # posts made during that month.
+        for month in sorted(files[o[0]], reverse=True):
+            # Add a link to the month, to the year file it belongs to.
+            year_fd.write("<div><a href=\"%s\">%s</a></div>" % (o[0]+"-"+month+".html", months[month]))
+            # Clear the 'month' file
+            open("./local/blog/"+o[0]+"-"+month+".html", "w", encoding=ENCODING).close()
+            month_fd = open("./local/blog/"+o[0]+"-"+month+".html", "a", encoding=ENCODING)
+            # Write the opening HTML tags
+            month_fd.write(content[2].replace("{{ title }}", "Post Archives - ").replace("{{ BODYID }}", "archives", 1).replace("<!--BLOCK HEADER-->", "<article>\n<p>\n"+months[month]+", <a href=\""+o[0]+".html\">"+o[0]+"</a>\n</p>\n</article>", 1))
+
+            # Sort the sub-dictionaries by keys, days, then iterate over it.
+            for day in sorted(files[o[0]][month], reverse=True):
+                # Sort the sub-dictionaries by keys, timestamps, then iterate over it
+                for timestamp in sorted(files[o[0]][month][day], reverse=True):
+                    # If a structure file already exists, don't rebuild the HTML file for individual articles
+                    if (not isfile("./local/blog/"+files[o[0]][month][day][timestamp].lower().replace(" ","-")[0:-3]+"html")):
+                        GenPage(files[o[0]][month][day][timestamp], "%s/%s/%s %s" % (o[0], month, day, timestamp))
+                    else:
+                        if (CompareMtimes("./Content/"+files[o[0]][month][day][timestamp], "./local/blog/"+files[o[0]][month][day][timestamp].lower().replace(" ","-")[0:-3]+"html")):
+                            pass
+                        else:
+                            # Generate each content file. "year", "month", "day", "timestamp"
+                            # identify the file in the dictionary, and the passed time values
+                            # designate the desired update time to set the content file.
+                            GenPage(files[o[0]][month][day][timestamp], "%s/%s/%s %s" % (o[0], month, day, timestamp))
+
+                    # For each article made in the month, add an entry on the appropriate
+                    # 'month' structure file.
+                    month_fd.write("<article>\n    %s<a href=\"%s\">%s</a>\n</article>\n" % (o[0]+"/"+month+"/"+day+" "+timestamp+": ", files[o[0]][month][day][timestamp].lower().replace(" ", "-")[0:-3]+"html", trip()))
+
+            # Write closing HTML tags to the month file.
+            month_fd.write(content[1].replace("assets/", "../assets/"))
+            month_fd.close()
+
+        # Write closing HTML tags to the year file.
+        year_fd.write("</div>\n"+content[1].replace("assets/", "../assets/"))
+        year_fd.close()
+
+        # Cleanup
+        del year_fd, month_fd
 
 
 
