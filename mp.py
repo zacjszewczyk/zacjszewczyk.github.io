9feb9faf3d889b6709a6545a6eca7b78f7f2e738
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2019-12-07 19:02:40 -0500

    .

--- mp.py
+++ mp.py
@@ -900,12 +900,16 @@ if __name__ == '__main__':
     t1 = datetime.now()
 
     Init()
-    GenStatic()
-    with Pool() as pool:
-        pool.map(Orchestrator, files.items())
+    # GenStatic()
+    
+    for each in files.items():
+        print(each)
+        print()
 
-    GenBlog()
-    Terminate()
+    # with Pool() as pool:
+    #     pool.map(Orchestrator, files.items())
+    # GenBlog()
+    # Terminate()
 
     t2 = datetime.now()
     print("Execution time: "+str(t2-t1)+"s")
