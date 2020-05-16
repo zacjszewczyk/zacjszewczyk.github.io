70124f37b59810f1e1401af27a1869ef39c08043
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2020-05-16 09:18:45 -0400

    Added more categories

--- cats.py
+++ cats.py
@@ -54,4 +54,95 @@ def migrate():
 
     print("Categories:",categories)
 
-categories = ['Tech', 'Politics', 'Uncategorized', 'Finances', 'Writing', 'Programming', 'Podcasts', 'Simple living', 'Readme', 'Gear', 'Travel', 'Motivation', 'Overlanding', 'Weightlifting', 'Blogging', 'Politcs', 'Preparedness']
+### Import modules
+from sys import exit, argv, stdout, stdin # Command line interface
+from tty import setraw, setcbreak # Raw input
+from termios import tcgetattr, tcsetattr, TCSAFLUSH # Backup/resume shell
+from os.path import exists # Reading input files
+from os import popen # Detect terminal size
+from re import sub # Change menu to try to avoid text wrapping
+
+# Method: GetUserSelection
+# Purpose: Display a menu for the user, allowing for cursor movement.
+# Parameters:
+# - prompt: Items to prompt the user with (List)
+def GetUserSelection(options):
+        # Bounds check
+        if (len(options) < 2):
+                print(c.FAIL+"Error: Not enough options."+c.ENDC)
+
+        # Backup the shell session, to restore it later. Set the stage.
+        backup = tcgetattr(stdin)
+        setraw(stdin)
+        index = 0
+
+        # Continue accepting input until the user aborts the process (CTRL-C)
+        # or hits the enter key.
+        while True:
+                # Print menu
+                for i, each in enumerate(options):
+                        if (i == index):
+                                stdout.write(u"\u25C9 %s\u001b[1000D\n" % (each))
+                        else:
+                                stdout.write(u"\u25EF %s\u001b[1000D\n" % (each))
+                stdout.write(u"\u001b[1000D")
+
+                # Read a single character and get its code.
+                char = ord(stdin.read(1))
+
+                # Manage internal data-model
+                if char == 3: # CTRL-C
+                        # Restore shell session from backup, then exit.
+                        tcsetattr(stdin, TCSAFLUSH, backup)
+                        return
+                elif char in {10, 13}: # Enter key
+                        # Restore shell session from backup, then exit the loop.
+                        tcsetattr(stdin, TCSAFLUSH, backup)
+                        break
+                elif char == 27: # Arrow keys
+                        # Accept input from arrow keys and move the cursor accordingly
+                        next1, next2 = ord(stdin.read(1)), ord(stdin.read(1))
+                        if next1 == 91:
+                                if next2 == 65: # Up
+                                        index = max(index-1,0)
+                                elif next2 == 66: # Down
+                                        index = min(index+1,len(options)-1)
+                # Clear menu
+                for i in range(1,len(options)+1):
+                        stdout.write(u"\u001b[1A")
+                        stdout.write(u"\u001b[0J")
+        return options[index]
+
+categories = ['Tech', 'Politics', 'Uncategorized', 'Finances', 'Writing', 'Programming', 'Podcasts', 'Simple living', 'Readme', 'Gear', 'Travel', 'Motivation', 'Overlanding', 'Weightlifting', 'Blogging', 'Politcs', 'Preparedness', "Cybersecurity", "Personal Development", "Professional Development"]
+
+for each in listdir("./content"):
+    each = f"./content/{each}"
+    if (each[-4:] != ".txt"): continue
+
+    fd = open(each,"r")
+    if ("Category" in {y[0].strip():y[1].strip() for y in [x.split(": ") for x in fd.readlines()[0:6] if x != '\n']}.keys()):
+        continue
+
+    print(f"Choosting for {each}")
+
+    cat = GetUserSelection(categories)
+    if (cat == None):
+        break
+
+    print(f"Chose '{cat}' for '{each}'")
+
+    f = open(each, "r")
+    contents = f.readlines()
+    f.close()
+
+    contents.insert(4, f"Category: {cat}\n")
+
+    f = open(each, "w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
+    fd = open(each, "r")
+    mtime = mktime(strptime(fd.readlines()[3][9:].strip(), "%Y/%m/%d %H:%M:%S"))
+    fd.close()
+    utime(each, (mtime, mtime))
